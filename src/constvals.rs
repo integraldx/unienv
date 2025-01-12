9c6b89c86d23c71ad115be5cbe99d43631da9d98
integraldx <integraldx@integraldx.me>
2025-01-13 00:54:59 +0900

    add: default unity path

---
+++ src/constvals.rs
@@ -0,0 +1,1 @@
+pub const WINDOWS_UNITY_DEFAULT_BASE_PATH: &str = "C:\\Program Files\\Unity\\Hub\\Editor\\2022.3.47f1\\Editor\\Unity.exe";
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::process::Command;
+
+mod constvals;
+
+fn main() -> Result<(), std::io::Error>{
     println!("Hello, world!");
+
+    let mut command = Command::new(constvals::WINDOWS_UNITY_DEFAULT_BASE_PATH);
+
+    let output = command.output();
+
+    return match output {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e),
+    };
 }
